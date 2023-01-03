58d2d2beda8313e4a7e06df5bbbc6884c88b208b
Hyyped <110073191+Hyyped@users.noreply.github.com>
2023-01-03 09:26:36 -0500

    added syntax error api to calculators

--- tool.js
+++ tool.js
@@ -5,7 +5,14 @@ for (;;) {
             let factor1 = window.prompt("Enter the first factor");
             let factor2 = window.prompt("Enter the second factor"); 
             let product = Number(factor1) * Number(factor2); 
-            alert(product);
+            if (isNaN(product)) {
+                product = "error.invaldSyntax";
+                alert(product) 
+            }
+            else {
+                product = Number(factor1) * Number(factor2);
+                alert(product)
+            }
             if(confirm("Cancel to end process") == false) {
                 throw error("Calculation Completed");
             }
@@ -14,7 +21,14 @@ for (;;) {
             let dividend = window.prompt("Enter the dividend"); 
             let divisor = window.prompt("Enter the divisor"); 
             let quotient = Number(dividend) / Number(divisor); 
-            alert(quotient);
+            if (isNaN(quotient)) {
+                quotient = "error.invaldSyntax";
+                alert(quotient) 
+            }
+            else {
+                let quotient = Number(dividend) / Number(divisor); 
+                alert(quotient)
+            }
             if(confirm("Cancel to end process") == false) {
                 throw error("Calculation Completed");
             }
@@ -23,7 +37,14 @@ for (;;) {
             let digit1add = window.prompt("Enter Digit 1"); 
             let digit2add = window.prompt("Enter Digit 2"); 
             let sum = Number(digit1add) + Number(digit2add);
-            alert(sum);
+            if (isNaN(sum)) {
+                sum = "error.invaldSyntax";
+                alert(sum) 
+            }
+            else {
+                let sum = Number(digit1add) + Number(digit2add);
+                alert(sum)
+            }
             if(confirm("Cancel to end process") == false) {
                 throw error("Calculation Completed");
             }
@@ -32,7 +53,14 @@ for (;;) {
             let minuend = window.prompt("Enter Digit 1"); 
             let subtrahend = window.prompt("Enter digit 2"); 
             let difference = minuend - subtrahend;
-            alert(difference);
+            if (isNaN(difference)) {
+                difference = "error.invaldSyntax";
+                alert(difference) 
+            }
+            else {
+                let difference = minuend - subtrahend;
+                alert(difference)
+            }
             if(confirm("Cancel to end process") == false) {
                 throw error("Calculation Completed");
             }
@@ -41,7 +69,14 @@ for (;;) {
             let base = window.prompt("Enter the base"); 
             let exponent = window.prompt("Enter the exponent"); 
             let solvedExponent = Number(base) ** Number(exponent);
-            alert(solvedExponent);
+            if (isNaN(solvedExponent)) {
+                solvedExponent = "error.invaldSyntax";
+                alert(solvedExponent) 
+            }
+            else {
+                let solvedExponent = Number(base) ** Number(exponent);
+                alert(solvedExponent)
+            }
             if(confirm("Cancel to end process") == false) {
                 throw error("Calculation Completed");
             }
