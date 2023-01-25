99bc3c0e64038a99a8149cd757e37c716c471b97
Hyyped <110073191+Hyyped@users.noreply.github.com>
2023-01-25 10:17:25 -0500

    percentages program :)

--- tool.js
+++ tool.js
@@ -327,4 +327,71 @@ if(confirm("Confirm to select a program\nCurrent program: Metric Calculator") ==
     };
 };
 };
+if(confirm("Confirm to select a program\nCurrent program: Percentage Finder") == true) {
+    for(;;) {
+        if (confirm("Confirm to select a percentage finder\nCurrent conversion: number to percent") == true) {
+            var number = prompt("Enter a number");
+            percentage = number * 100;
+            if (isNaN(percentage)) {
+                alert("error.invalidSyntax");
+            }
+            else {
+                alert(number + " is equal to " + percentage + "%");
+            };
+            if (confirm("Cancel to end process") == false) {
+                throw error("Calculation Completed");
+            }
+        };
+        if (confirm("Confirm to select a percentage finder\nCurrent conversion: fraction to percent") == true) {
+            var numerator = prompt("Enter the numerator");
+            var denominator = prompt("Enter the denominator");
+            var diff = 100 / denominator;
+            var percentage = numerator * diff;
+            if (isNaN(percentage)) {
+                alert("error.invalidSyntax");
+            }
+            else {
+                alert(numerator + "/" + denominator + " is equal to " + percentage + "%");
+            };
+            if (confirm("Cancel to end process") == false) {
+                throw error("Calculation Completed");
+            }
+        };
+        if (confirm("Confirm to select a percentage finder\nCurrent conversion: percentage to number or fraction") == true) {
+            var percentage = prompt("Enter a percentage");
+            var numerator = percentage;
+            var denominator = 100;
+            var number = percentage / 100;
+            var nFrac = numerator;
+            var dFrac = denominator;
+            var fracV = 1;
+            simpFrac();
+            numerator = nFrac;
+            denominator = dFrac;
+            alert(percentage + "% is equal to " + number + " and " + numerator + "/" + denominator + ".");
+            if (confirm("Cancel to end process") == false) {
+                throw error("Calculation Completed");
+            }
+        };
+        if (confirm("Confirm to select a percentage finder\nCurrent finder: find percentage of number") == true) {
+            var number = prompt("Enter a number");
+            var percentage = prompt("Enter a percentage");
+            var diff = 100 / percentage;
+            var vEnd = number / diff;
+            alert(percentage + "% of " + number + " is " + vEnd + ".");
+            if (confirm("Cancel to end process") == false) {
+                throw error("Calculation Completed");
+            }
+        };
+    };
+};
+function simpFrac() {
+    for (var i = 1; i < dFrac; i++) {
+        fracV = fracV + 1;
+        if (Number.isInteger(nFrac / fracV) && Number.isInteger(dFrac / fracV)) {
+          nFrac = nFrac / fracV;
+          dFrac = dFrac / fracV;  
+        };
+    };
+};
 };
