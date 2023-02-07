c2c7ebfbfadc765da59951690de3f2d21989dbbb
Hyyped <110073191+Hyyped@users.noreply.github.com>
2023-02-07 09:02:09 -0500

    re-added liscense and squished bug

--- percentage finder/get discount.js
+++ percentage finder/get discount.js
@@ -1,3 +1,11 @@
+/** 
+* @license ToolScript © 2023 by Hyyped is licensed under CC BY-NC-SA 4.0 
+* All Reproduction of this code MUST be a remix and attribution is required 
+* Any PUBLIC copy of this work is not legal and a DMCA takedown of said repo or website will be submitted
+* WARNING:
+* I am not responsible for any trouble you get in for using any of the scripts included in ToolScript© 
+**/
+
 var price = prompt("Enter a price (no $ sign)");
 var percentOff = prompt("Enter a discount (% off)");
 var newPrice = (price * (percentOff / 100));
